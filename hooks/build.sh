655a381277306b4dac28d6774cede143d6a59e52
Duncan de Boer <duncan@charpand.nl>
2020-05-02 22:28:13 +0200

    trying docker auto push

--- hooks/build.sh
+++ hooks/build.sh
@@ -11,11 +11,16 @@ docker build . \
     --build-arg version=$VERSION \
     --build-arg base_image=$BASE_IMAGE
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $TAG
+
 image_id=$(docker images $TAG --format "{{.ID}}")
 for tag in ${EXTRA_TAGS//;/$'\n'}
 do
     echo $tag
     docker tag $image_id "charpand/frontend:${tag}"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker push $image_id "charpand/frontend:${tag}"
 done
 
 docker run --rm --entrypoint echo "$TAG" "Hello $hello"
